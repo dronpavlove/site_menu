2aaa0146aea717e039e4e360a5e6d3c1d1bbd8b1
dronpavlove <pavlove75@mail.ru>
2023-03-03 23:45:10 +0300

    рабочий код

--- site_menu/mymenu/urls.py
+++ site_menu/mymenu/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import CategoryMenuListView, get_objects_menu, get_my_menu
+from .views import CategoryMenuListView, get_my_menu
 
 urlpatterns = [
     path('', CategoryMenuListView.as_view(), name='category-menu-list'),
-    # path('<str:slug>/', get_objects_menu, name='podcategory-menu'),
     path('<str:slug>/', get_my_menu, name='podcategory-menu'),
 ]
--- site_menu/mymenu/views.py
+++ site_menu/mymenu/views.py
@@ -8,24 +8,5 @@ class CategoryMenuListView(ListView):
     template_name = "mymenu/menu_list.html"
 
 
-def get_objects_menu(request, **kwargs):
-    data = {}
-    if request.method == 'GET':
-        data['filter_menu'] = CategoryMenu.objects.filter(slug=kwargs['slug']).select_related('parent').prefetch_related('children').order_by('tree_id')
-        filter_menu = data['filter_menu'][0]
-        data['title'] = filter_menu.title
-        data['categories'] = [i for i in filter_menu.posts.all()]
-        data['parent'] = get_menu(filter_menu, punkt_list=[filter_menu])
-        return render(request, 'mymenu/podmenu_list.html', context=data)
-
-
-def get_menu(obj, punkt_list: list):
-    if obj.parent:
-        punkt_list.append(obj.parent)
-        return get_menu(obj.parent, punkt_list)
-    else:
-        return punkt_list[::-1]
-
-
 def get_my_menu(request, **kwargs):
     return render(request, 'mymenu/menu.html', {'slug': kwargs['slug']})
